c47ff2a65a7968558fc7606baa9b23ad1151071e
Yann Zerlaut <yann.zerlaut@gmail.com>
2020-01-13 16:05:45 +0100

    starting analysis module

---
+++ fairgraph/analysis.py
@@ -0,0 +1,47 @@
+"""
+analysis
+"""
+
+try:
+    basestring
+except NameError:
+    basestring = str
+import os.path
+import logging
+from datetime import datetime, date
+import mimetypes
+from itertools import chain
+import sys
+import inspect
+from dateutil import parser as date_parser
+import requests
+from .base import KGObject, cache, KGProxy, build_kg_object, Distribution, as_list, KGQuery, Field, IRI
+from .commons import BrainRegion, CellType, Species, AbstractionLevel, ModelScope, OntologyTerm
+from .core import Organization, Person, Age, Collection
+from .utility import compact_uri, standard_context
+from .model_validation import ATTACHMENT_SIZE_LIMIT, HasAliasMixin
+
+logger = logging.getLogger("fairgraph")
+mimetypes.init()
+
+DEFAULT_NAMESPACE = "modelvalidation"
+
+class Analysis():
+    pass
+
+
+class AnalysisConfiguration():
+    pass
+
+
+
+def list_kg_classes():
+    """List all KG classes defined in this module"""
+    return [obj for name, obj in inspect.getmembers(sys.modules[__name__])
+            if inspect.isclass(obj) and issubclass(obj, KGObject) and obj.__module__ == __name__]
+
+
+def use_namespace(namespace):
+    """Set the namespace for all classes in this module."""
+    for cls in list_kg_classes():
+        cls.namespace = namespace
